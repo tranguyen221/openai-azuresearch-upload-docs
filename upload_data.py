ec6aa7b09fa5028935b0ffe0f0365be383005dd5
Trang <Nguyen.Trang@microsoft.com>
2023-09-12 17:14:51 +0200

    Implement streamlit demo

--- upload_data.py
+++ upload_data.py
@@ -2,15 +2,15 @@ import os
 import base64
 import fitz
 from dotenv import load_dotenv
+load_dotenv()
 import streamlit as st
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import AzureChatOpenAI
 import openai
-from langchain.chat_models import AzureChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import AzureSearch
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import AzureSearch
+
+
 
 
 def display_pdf(placeholder, pdf_bytes, page_number=None):
@@ -34,39 +34,38 @@ def load_docs(directory_path):
     return documents
 
 @st.cache_resource
-def get_vectorstore(directory_path, chunk_size=1000, chunk_overlap=20, separator="\n"):
+def index_documents(directory_path, chunk_size=1000, chunk_overlap=20, separator="\n"):
     """Load PDF document and create a vectorstore from the text chunks.
     Cache the result to avoid repeated calls to the embeddings model."""
-
-    # Configure OpenAI API
-    openai.api_type = "azure"
-    openai.api_base = os.getenv('AZURE_OPENAI_ENDPOINT')
-    openai.api_key = os.getenv('AZURE_OPENAI_KEY')
-    openai.api_version = os.getenv('AZURE_OPENAI_API_VERSION')
-
-    embeddings = OpenAIEmbeddings(
-        deployment=os.getenv("OPENAI_EMBEDDINGS_DEPLOYMENT_NAME"),
-        chunk_size=1,
-        request_timeout=120,
-    )
-
-    st.write(os.getenv('AZURE_SEARCH_SERVICE'))
-
-    # Connect to Azure Cognitive Search
-    acs = AzureSearch(azure_search_endpoint=f"https://{os.getenv('AZURE_SEARCH_SERVICE')}.search.windows.net",
-                    azure_search_key=os.getenv('AZURE_SEARCH_KEY'),
-                    index_name=os.getenv('AZURE_SEARCH_INDEX'),
-                    embedding_function=embeddings.embed_query)
+    st.write("Chunking documents...")
     docs = load_docs(directory_path)
     print(f"Number of documents: {len(docs)}")
-    # text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-    #     chunk_size=chunk_size, chunk_overlap=chunk_overlap, separator=separator
-    # )
-    # texts = text_splitter.split_documents(docs) 
+
+    openai.api_type = "azure"
+    openai.api_base = os.getenv("AZURE_OPENAI_BASE")
+    # openai.api_key = os.getenv("AZURE_OPENAI_EMBEDDING_KEY")
+    # openai.api_version = os.getenv("AZURE_OPENAI_API_VERSION")
+
+    openai.api_key = os.getenv("AZURE_OPENAI_EMBEDDING_KEY_BACKUP")
+    openai.api_version = os.getenv("AZURE_OPENAI_API_VERSION")
+
+    embeddings = OpenAIEmbeddings(
+            deployment_id=os.getenv("AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME_BACKUP"),
+            chunk_size=1,
+            request_timeout=120,
+        )
+    # Connect to Azure Cognitive Search
+    acs = AzureSearch(azure_search_endpoint=f"https://{os.getenv('AZURE_SEARCH_SERVICE')}.search.windows.net",
+                azure_search_key=os.getenv('AZURE_SEARCH_KEY'),
+                index_name=os.getenv('AZURE_SEARCH_INDEX'),
+                embedding_function=embeddings.embed_query)
+ 
     texts = split_docs(docs, chunk_size, chunk_overlap)
     print(f"Number of chunks: {len(texts)}")
+    st.write("Start to generate index to Azure Search...")
     # Add documents to Azure Search
     acs.add_documents(documents=docs)
+    st.write("Your index is ready! Please go to the chat page to start your conversation.")
 
 
 def load_docs(directory):
@@ -76,13 +75,11 @@ def load_docs(directory):
 
 
 def main():
-    load_dotenv()
-
     # set page config and slide bar
     st.set_page_config(layout="wide", page_title="Add your data...", page_icon="🔍")
     # set_sidebar()
 
-    st.write("<h1><center>Add your customize data</center></h1>", unsafe_allow_html=True)
+    st.write("<h1><center>Add your customized data</center></h1>", unsafe_allow_html=True)
 
     st.subheader("Upload document")
 
@@ -114,12 +111,10 @@ def main():
             run_button = st.button("Run")
             
             if run_button:
-
-                st.write("Start to generate index to Azure Search...")
-                get_vectorstore(
-                folder_path
+                
+                index_documents(
+                    folder_path
                 )
-                st.write("Your index is ready! Please go to the chat page to start your conversation.")
 
 
 if __name__ == "__main__":
